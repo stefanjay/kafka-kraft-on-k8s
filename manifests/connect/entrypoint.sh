eb30b7d992f6e57a4ce058516ab620c97d1ed076
Jaehrling, Stefan <stefan.jaehrling@sva.de>
2024-03-05 22:42:21 +0100

    added dynamic way to add parameter to config for kafka connect

--- manifests/connect/entrypoint.sh
+++ manifests/connect/entrypoint.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-exec connect-distributed.sh /opt/kafka/config/connect-distributed.properties
+CNF=/opt/kafka/config/connect-distributed.properties
+
+sed -e "s+^bootstrap.servers=.*+bootstrap.servers=$SERVICE.$NAMESPACE.svc:9092+" \
+    -e "s+^key.converter=.*+key.converter=$KEY_CONVERTER+" \
+    -e "s+^value.converter=.*+value.converter=$VALUE_CONVERTER+" \
+    -e "s+^key.converter.schemas.enable=.*+key.converter.schemas.enable=$KEY_CONVERTER_SCHEMAS_ENABLE+" \
+    -e "s+^value.converter.schemas.enable=.*+value.converter.schemas.enable=$VALUE_CONVERTER_SCHEMAS_ENABLE+" \
+    -e "s+^offset.storage.replication.factor=.*+offset.storage.replication.factor=$OFFSET_STORAGE_REPLICATION_FACTOR+" \
+    -e "s+^offset.storage.partitions=.*+offset.storage.partitions=$OFFSET_STORAGE_PARTITIONS+" \
+    -e "s+^offset.storage.cleanup.policy=.*+offset.storage.cleanup.policy=$OFFSET_STORAGE_CLEANUP_POLICY+" \
+    -e "s+^status.storage.replication.factor=.*+status.storage.replication.factor=$STATUS_STORAGE_REPLICATION_FACTOR+" \
+    -e "s+^status.storage.partitions=.*+status.storage.partitions=$STATUS_STORAGE_PARTITIONS+" \
+    -e "s+^listeners=.*+listeners=$LISTENERS+" \
+    -e "s+^plugin.path=.*+plugin.path=$PLUGIN_PATH+" \
+    $CNF > $CNF.updated \
+    && mv $CNF.updated $CNF
+
+exec connect-distributed.sh $CNF
